7f21887b2c2bd884eb7b44d47962db488ab51a7e
Joseph-Kung <joseph.kung@hotmail.com>
2018-10-29 14:58:26 -0700

    Create #bonus for Employee and Manager classes

--- class_inheritence/employee_mgr_exercise.rb
+++ class_inheritence/employee_mgr_exercise.rb
@@ -1,12 +1,26 @@
 class Employee
+  attr_reader :name, :title, :salary, :boss
+  
   def initialize(name, title, salary, boss)
     @name, @title, @salary, @boss = name, title, salary, boss
+  end
+  
+  def bonus(multiplier)
+    @salary * multiplier
   end
 end
 
 class Manager < Employee
+  attr_accessor :employees
+  
   def initialize(name, title, salary, boss, employees=[])
     super(name, title, salary, boss)
     @employees = employees
+  end
+  
+  def bonus(multiplier)
+    salaries = []
+    @employees.each {|employee| salaries << employee.salary}
+    salaries.reduce(:+) * multiplier
   end
 end
